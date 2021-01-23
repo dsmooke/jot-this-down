b8752201cb8f8fd0418611dfd35a6feaea2f0945
Danny Smooke <dana.smooke@gmail.com>
2021-01-23 09:44:24 -0500

    copied instructions into file, added similar starter code from activity14-finalstarwarsapp

--- app.js
+++ app.js
@@ -18,3 +18,50 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// 1. HTML Routes-------
+// 1a. GET `/notes` - Should return the `notes.html` file.
+
+// 1b. GET `*` - Should return the `index.html` file
+// Basic route that sends the user first to the AJAX Page
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
+// 3. API Routes------
+
+// 3a. GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+
+app.get("/api/notes", function (req, res) {
+    return res.json("db.json");
+});
+
+// 3b. POST `/api/notes` - should receive a new note to save on the request body,  add it to the `db.json` file, and then return the new note to the client--takes in JSON input
+
+app.post("/api/notes", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newNote = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newNote);
+
+    notes.push(newNote);
+
+    res.json(newNote);
+});
+
+// 3c. DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
