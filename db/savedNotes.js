5e9f92a915bb8836715a1f9cedf931da2113fb24
Danny Smooke <dana.smooke@gmail.com>
2021-01-24 18:37:32 -0500

    created savedNote.js that contains same info as db.json but added const savedNotes to be exported to app.js file, commented out a possible way to add in id for each note

---
+++ db/savedNotes.js
@@ -0,0 +1,12 @@
+const savedNotes = require("./db.json")
+// var addID = addID();
+
+[
+    {
+        // "id": addID(),
+        "title": "Test Title",
+        "text": "Test text"
+    }
+]
+
+module.exports = savedNotes
