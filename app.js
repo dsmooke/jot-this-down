8807987cea727096afa34cd6067c48c006bfa1d5
Danny Smooke <dana.smooke@gmail.com>
2021-01-24 18:33:35 -0500

    deleted comments of copied activity code

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-// Sets up Inquirer
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
-// Express Dependencies
 const express = require("express");
 const path = require("path");// path: ^ used for both Inquirer.js and Express.js
 
-
-// Sets up the Express App
 const app = express();
 
 // Set 'public' as static folder
@@ -31,7 +27,6 @@ app.use(logger);
 
 // 1. HTML Routes-------
 
-
 // 1a. GET `/notes` - Should return the `notes.html` file. 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "notes.html"));
@@ -51,29 +46,16 @@ app.get("*", function (req, res) {
 // 3a. GET `/api/notes` - Should read the `db.json` file (will contain all created notes) and return all saved notes as JSON. (Rest API)
 
 app.get("/api/notes", function (req, res) {
-    return res.json("db.json"); //rename db.json to savedNotes.json? and module.exports = savedNotes ?
 });
 
 // 3b. POST `/api/notes` - should receive a new note to save on the request body,  add it to the `db.json` file, and then return the new note to the client--takes in JSON input
 
 // app.post("/api/notes", function (req, res) {
 
-// req.body hosts is equal to the JSON post sent from the user
-// This works because of our body parsing middleware
-// var newNote = req.body;
-
-// Using a RegEx Pattern to remove spaces from newCharacter
-// You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-// newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
-
-// console.log(newNote);
-
-// notes.push(newNote);
-
-// res.json(newNote);
-// });
 
 // 3c. DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+
 
 // Starts the server to begin listening
 app.listen(PORT, function () {
