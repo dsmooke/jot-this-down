d94bb13ee9e44788e227864553077d1de769f34a
Danny Smooke <dana.smooke@gmail.com>
2021-01-25 08:37:54 -0500

    temporarily deleted post and delete request instructions, removed explanation comments

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const inquirer = require("inquirer");
+// const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
 const express = require("express");
-const path = require("path");// path: ^ used for both Inquirer.js and Express.js
+const path = require("path");
 
 const app = express();
 
@@ -24,16 +24,10 @@ app.use(logger);
 
 // API Routes
 
-// GETs all created notes, returns as JSON. (Rest API)
+// Gets all created notes, returns as JSON. (Rest API)
 app.get("/api/notes", function (req, res) {
-    return res.json(notes); //rename db.json to savedNotes.json? and module.exports = savedNotes ?
+    return res.json(notes);
 });
-
-// POST `/api/notes` - should receive a new note to save on the request body,  add it to the `db.json` file, and then return the new note to the client--takes in JSON input
-
-// DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-
-
 
 // Starts the server to begin listening
 app.listen(PORT, function () {
