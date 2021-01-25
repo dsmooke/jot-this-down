27cb7b7416a63dc5132c1ffca1e33c7c60774ce0
Danny Smooke <dana.smooke@gmail.com>
2021-01-25 09:01:33 -0500

    added two test notes with test ids of 1 and 2, test access with postman - works

--- Notes.js
+++ Notes.js
@@ -1,6 +1,12 @@
 const notes = [
     {
-        "id": "Test ID",
+        "id": 1,
+        "title": "Test Title",
+        "text": "Test text"
+    },
+
+    {
+        "id": 2,
         "title": "Test Title",
         "text": "Test text"
     }
