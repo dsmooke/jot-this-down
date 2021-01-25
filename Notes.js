6d0da97b82406655ca581b5c5a73698fab22bda5
Danny Smooke <dana.smooke@gmail.com>
2021-01-25 10:33:31 -0500

    deleted second test note

--- Notes.js
+++ Notes.js
@@ -3,12 +3,6 @@ const notes = [
         "id": 1,
         "title": "Test Title",
         "text": "Test text"
-    },
-
-    {
-        "id": 2,
-        "title": "Test Title",
-        "text": "Test text"
     }
 ]
 
